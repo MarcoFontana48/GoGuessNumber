224fc2674729c89ee4a2532e3e0efba63f0bffe5
Marco Fontana <marco.fontana17@studio.unibo.it>
2024-05-13 20:10:43 +0200

    refactor: removed 'seed' from rand func

--- src/main.go
+++ src/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"math/rand"
-	"time"
-)
-
 const (
 	MAX = 100 // Max secret number
 	N   = 10  // Number of players
@@ -13,8 +8,6 @@ const (
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
 	// creates a new oracle 'master'
 	oracle := NewOracle()
 
